f645666d79c59bb97ee288ef77dbcfb78ce14ab4
DESKTOP-3TGS524\Benjamin <benjamin.le.roux.lfp@gmail.com>
2017-03-11 10:32:08 +0100

    first commit

---
+++ Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(GameControl.instance.scrollSpeed, 0);
+    }
+
+    private void Update()
+    {
+        if (GameControl.instance.gameOver) { rb2d.velocity = Vector2.zero; }
+    }
+}
